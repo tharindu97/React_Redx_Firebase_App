e31f955ea3dcbdc28e47685b4dc1a0693ad88037
tharindu97 <tharindukavishna@gmail.com>
2019-11-23 15:15:55 +0530

    4_5_6

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>tharindu</h1>
+        <Navbar/>
+        <Switch>
+          <Route path='/' component = {Dashboard} />
+        </Switch>
       </div>  
     </BrowserRouter>
   );
---
+++ marioplan/src/components/projects/ProjectSummary.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProjectSummary = () =>{
+    return(
+        <div className="card z-depth-0 project-summary">
+                <span className="card-title">Project Title</span>
+                <p>Posted by the Net Tharindu</p>
+                <p className="grey-text">3rd September, 2am</p>
+        </div>
+    )
+}
+
+export default ProjectSummary
