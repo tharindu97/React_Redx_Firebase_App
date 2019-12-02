c7f42aa1d3d85164b6b2c782057b67e16223bbd7
tharindu97 <tharindukavishna@gmail.com>
2019-12-02 11:45:45 +0530

    9_10_11_12_13

--- marioplan/src/components/projects/ProjectSummary.js
+++ marioplan/src/components/projects/ProjectSummary.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const ProjectSummary = () =>{
+const ProjectSummary = ({project}) =>{
     return(
         <div className="card z-depth-0 project-summary">
-                <span className="card-title">Project Title</span>
+                <span className="card-title">{project.title}</span>
                 <p>Posted by the Net Tharindu</p>
                 <p className="grey-text">3rd September, 2am</p>
         </div>
---
+++ marioplan/src/store/reducers/projectReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+    projects:[
+        {id: '1', title: 'help me find peach', content: 'blah blah blah'},
+        {id: '2', title: 'help me find peach 2', content: 'blah blah blah'},
+        {id: '3', title: 'help me find peach 3', content: 'blah blah blah'}
+    ]
+}
+
+const projectReducer = (state = initState, action) => {
+    return state
+}
+
+export default projectReducer
